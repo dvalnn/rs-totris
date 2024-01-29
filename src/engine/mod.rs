498816fb9dfc8a2e20fa714843b6ace54ecec6b8
Tiago Amorim <tiago.andre.amorim@gmail.com>
2024-01-29 16:27:22 +0000

    struct engine prototyping

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,3 +1,25 @@
 #![allow(dead_code, unused_variables)]
 
-struct Engine{}
+struct Engine {
+    board: Board,
+}
+
+impl Engine {
+    fn new() -> Self {
+        Engine {
+            board: Board::new()
+        }
+    }
+}
+
+struct Board([bool; Board::SIZE]);
+
+impl Board{
+    const WIDTH: usize = 10;
+    const HEIGHT: usize = 10;
+    const SIZE: usize = Self::WIDTH * Self::HEIGHT;
+
+    fn new() -> Self{
+        Self([false; Self::SIZE])
+    }
+}
