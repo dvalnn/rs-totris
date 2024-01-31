a514a13d51c0ed451e72c3a9c05a2e0e82b49656
Tiago Amorim <tiago.andre.amorim@gmail.com>
2024-01-31 22:54:47 +0000

    movement related functionality

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,27 +1,32 @@
-#![allow(dead_code, unused_variables)]
-
+mod matrix;
 mod piece;
 
+use matrix::Matrix;
 use piece::{Kind as PieceKind, Piece};
 
-type Coordinate = cgmath::Vector2<usize>;
+type Coordinate = cgmath::Point2<usize>;
 type Offset = cgmath::Vector2<isize>;
 
+#[rustfmt::skip]
+pub enum MoveKind { Left, Right, }
+
+impl MoveKind {
+    fn offset(&self) -> Offset {
+        match self {
+            Self::Left => Offset::new(-1, 0),
+            Self::Right => Offset::new(1, 0),
+        }
+    }
+}
+
 pub struct Engine {
-    board: Board,
+    matrix: Matrix,
     bag: Vec<PieceKind>,
     cursor: Option<Piece>,
 }
 
+//NOTE: Private functions impl block
 impl Engine {
-    pub fn new() -> Self {
-        Engine {
-            board: Board::new(),
-            bag: Vec::new(),
-            cursor: None,
-        }
-    }
-
     fn refill_bag(&mut self) {
         debug_assert!(self.bag.is_empty());
         use rand::seq::SliceRandom;
@@ -38,40 +43,69 @@ impl Engine {
             .take()
             .expect("Place cursor called without cursor");
 
-        for coords in cursor.cells().expect("cursor out of bounds") {
-            let cell = self
-                .board
-                .get_mut(coords)
-                .expect("cursor out of bounds ?!?!?!");
+        debug_assert!(
+            !self.matrix.is_placeable(&cursor),
+            "Tried to place cursor in unplaceable location: {:?}",
+            cursor
+        );
 
-            // NOTE: The calling code should ensure this never happens
-            debug_assert!(!(*cell));
-            *cell = true;
+        for coords in cursor.cells().expect("cursor out of bounds !??!?!") {
+            self.matrix[coords] = true;
         }
+    }
+
+    fn ticked_down_cursor(&self) -> Option<Piece> {
+        let Some(cursor) = self.cursor else {
+            return None;
+        };
+        let new_cursor = cursor.moved_by(Offset::new(0, -1));
+        (!self.matrix.is_clipping(&new_cursor)).then_some(new_cursor)
     }
 }
 
-pub struct Board([bool; Board::SIZE]);
-
-impl Board {
-    const WIDTH: usize = 10;
-    const HEIGHT: usize = 10;
-    const SIZE: usize = Self::WIDTH * Self::HEIGHT;
-
-    fn new() -> Self {
-        Self([false; Self::SIZE])
+//NOTE: Public functions impl block
+impl Engine {
+    pub fn new() -> Self {
+        Engine {
+            matrix: Matrix::new(),
+            bag: Vec::new(),
+            cursor: None,
+        }
     }
 
-    const fn index(Coordinate { x, y }: Coordinate) -> usize {
-        x + y * Self::WIDTH
+    pub fn cursor_has_hit_bottom(&self) -> bool {
+        self.ticked_down_cursor().is_none()
     }
 
-    pub fn in_bounds(Coordinate { x, y }: Coordinate) -> bool {
-        x < Self::WIDTH && y < Self::HEIGHT
+    pub fn move_cursor(&mut self, kind: MoveKind) -> Result<(), ()> {
+        let Some(cursor) = self.cursor.as_mut() else {
+            return Ok(());
+        };
+
+        let new_cursor = cursor.moved_by(kind.offset());
+        let Some(cells) = new_cursor.cells() else {
+            return Err(());
+        };
+
+        if self.matrix.is_clipping(&new_cursor) {
+            return Err(());
+        }
+
+        self.cursor = Some(new_cursor);
+        Ok(())
     }
 
-    pub fn get_mut(&mut self, coordinate: Coordinate) -> Option<&mut bool> {
-        Self::in_bounds(coordinate)
-            .then(|| &mut self.0[Self::index(coordinate)])
+    pub fn tick_down(&mut self) {
+        self.cursor = Some(
+            self.ticked_down_cursor()
+                .expect("tried to tick down to invalid position"),
+        );
+    }
+
+    pub fn hard_drop(&mut self) {
+        while let Some(new_cursor) = self.ticked_down_cursor() {
+            self.cursor = Some(new_cursor);
+        }
+        self.place_cursor()
     }
 }
