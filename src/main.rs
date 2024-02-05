45ab2c1d04684dfdd79bdd5f194b38e871927d22
Tiago Amorim <tiago.andre.amorim@gmail.com>
2024-02-05 22:17:32 +0000

    removed debug code

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,8 @@
 mod engine;
 mod interface;
 
-use engine::{Color, Engine, Matrix};
-
+use engine::Engine;
 fn main() {
     println!("Hello, world!");
-    //TODO: Remove this
-    let mut matrix = Matrix::new();
-    for col in 0..=6 {
-        matrix[(col, 0).into()] = Some(Color::Green);
-    }
-
-    let mut engine = Engine::from_matrix(matrix);
-    engine.debug_add_cursor();
-    interface::run(engine);
+    interface::run(Engine::new());
 }
